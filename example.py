52c2780f2ff05fa39391853b7fd75d5978081114
very-unique-username <dgm82382@gmail.com>
2021-05-31 11:57:46 +0530

    Update example

--- example.py
+++ example.py
@@ -11,17 +11,15 @@ from main import Miner
 
 my_miner = Miner(number = 1,
                  channel = 1234567890,
-                 token = "IOUy2y8udHW2-23fdsf",
-                 owner_id = 1234567890
+                 token = "IOUy2y8udHW2-23fdsf"
             )
-            
+
 #The 'number' is the serial number of the miner. In this example, this is the first miner, if you have multiple miners, number them accordingly. I recommend using 3-5 miners for a high income.
 
 #The channel is the ID of the channel the commands will be sent in.
 
 #The token is your account token.
 
-#The owner_id is your account ID.
 #If you don't know how to find the IDs and token, look it up on YouTube, it's quite easy
 my_miner.run()
 
