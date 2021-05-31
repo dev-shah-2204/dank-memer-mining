ba379c00a159b8c71fc0225831948b19761d6691
very-unique-username <dgm82382@gmail.com>
2021-05-31 11:12:26 +0530

    Update example

--- example.py
+++ example.py
@@ -9,7 +9,12 @@ The main.py file and this file need to be in the same folder.
 """
 from main import Miner
 
-my_miner = Miner(number = 1, channel = 1234567890, token = "IOUy2y8udHW2-23fdsf", owner_id = 1234567890)
+my_miner = Miner(number = 1,
+                 channel = 1234567890,
+                 token = "IOUy2y8udHW2-23fdsf",
+                 owner_id = 1234567890
+            )
+            
 #The 'number' is the serial number of the miner. In this example, this is the first miner, if you have multiple miners, number them accordingly. I recommend using 3-5 miners for a high income.
 
 #The channel is the ID of the channel the commands will be sent in.
